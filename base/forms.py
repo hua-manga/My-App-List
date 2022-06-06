4eb0f14f4b6317de6c3d8fb19e04433fb8ff98a3
Tarea3HTML <victoria.rachelacosta@gmail.com>
2022-06-06 00:23:50 -0300

    proyecto intermedio

---
+++ base/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+# py reaordenar el formulario y la vista, la linda no la fea XD jajaja
+
+
+class PositionForm(forms.Form):
+    position = forms.CharField()
---
+++ base/tests.py
@@ -0,0 +1,3 @@
+from django.test import TestCase
+
+# aca tengo que agregar los tests de la app.
